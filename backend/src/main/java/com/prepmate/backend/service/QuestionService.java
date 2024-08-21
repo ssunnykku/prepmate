26f7c0ebdfcf74dbce5eb8d6b28842d27fac7909
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-08-22 00:59:14 +0900

    chore: QuestionController, QuestionService 주석 입력

--- backend/src/main/java/com/prepmate/backend/service/QuestionService.java
+++ backend/src/main/java/com/prepmate/backend/service/QuestionService.java
@@ -20,6 +20,10 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
     private final InterviewRepository interviewRepository;
 
+    /**
+     * 문제 추가
+     * @param questionReqDTO 문제 추가시 입력 내용(question, answer, interviewId)
+     */
     public void addQuestion(QuestionReqDTO questionReqDTO) {
             Interview interview = interviewRepository.findById(questionReqDTO.getInterviewId())
                     .orElseThrow(() -> new EntityNotFoundException("Interview not found with id: " + questionReqDTO.getInterviewId()));
@@ -30,6 +34,12 @@ public class QuestionService {
                     .interview(interview)
                     .build());
     }
+
+    /**
+     * 문제 조회
+     * @param questionId
+     * @return QuestionDTO
+     */
     public QuestionDTO getQuestion(Long questionId) {
 
         Question data = questionRepository.findById(questionId)
@@ -42,10 +52,14 @@ public class QuestionService {
                    .createdAt(data.getCreatedAt())
                    .build();
 
-
       return question;
     }
 
+    /**
+     * 문제 수정
+     * @param questionId
+     * @param questionReqDTO 문제 수정시 입력 내용(question, answer)
+     */
     public void setQuestion(Long questionId, QuestionReqDTO questionReqDTO) {
         Question data = questionRepository.findById(questionId)
                 .orElseThrow(() -> new EntityNotFoundException("Question not found with id: " + questionId));
@@ -55,6 +69,10 @@ public class QuestionService {
         questionRepository.save(data);
     }
 
+    /**
+     * 문제 리스트 조회
+     * @return QuestionDTO 리스트 반환
+     */
     public List<QuestionDTO> getQuestionList() {
         List<Question> questions = questionRepository.findAll();
         List<QuestionDTO> result = new ArrayList<>();
@@ -71,6 +89,10 @@ public class QuestionService {
         return result;
     }
 
+    /**
+     * 문제 삭제
+     * @param questionId
+     */
     public void removeQuestion(Long questionId) {
         questionRepository.deleteById(questionId);
     }
