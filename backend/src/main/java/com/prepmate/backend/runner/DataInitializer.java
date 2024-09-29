27c08c93251c140d1cbbbca00a6505db5050dc75
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-09-29 13:18:53 +0900

    refactor: create default user -> 생성 확인 검증 추가

--- backend/src/main/java/com/prepmate/backend/runner/DataInitializer.java
+++ backend/src/main/java/com/prepmate/backend/runner/DataInitializer.java
@@ -3,18 +3,25 @@ package com.prepmate.backend.runner;
 import com.prepmate.backend.domain.User;
 import com.prepmate.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.save(User.builder()
-                .name("서니").email("sun@gmail.com")
-                .password("1234").build());
+        if (userRepository.findByName("서니") == null) {
+            userRepository.save(User.builder()
+                    .name("서니").email("sun@gmail.com")
+                    .password("1234").
+                    build());
+        }
+        log.info("userId= {}", userRepository.findByName("서니").getUserId());
     }
 }
